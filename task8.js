a9fe0da0cdfda8e7190da8e3b251d5a9c83bd706
Volodymyr882 <110340297+Volodymyr882@users.noreply.github.com>
2022-08-01 11:14:58 +0300

    Create task8.js

---
+++ task8.js
@@ -0,0 +1,16 @@
+let secondsMinute=60;
+let minutesHour=60;
+let secondsHour=secondsMinute*minutesHour;
+console.log(secondsHour);
+
+let hoursDay=24;
+let secondsDay=secondsHour*hoursDay;
+console.log(secondsDay);
+
+let dayMoom=30;
+let secondMoon=secondsDay*dayMoom;
+console.log(secondMoon);
+
+alert("Second in hour-"+secondsHour);
+alert("Second in day-"+secondsDay);
+alert("Second in moon-"+secondMoon);
