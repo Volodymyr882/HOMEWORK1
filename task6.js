a73410a4f27e5c672b66de76f20095b0efc573c9
Volodymyr882 <110340297+Volodymyr882@users.noreply.github.com>
2022-08-01 11:14:09 +0300

    Create task6.js

---
+++ task6.js
@@ -0,0 +1,19 @@
+
+let myName="Vova";
+let surname="Kondratskyi";
+let namegroup=undefined;
+let yearOfBirth=1996;
+const maritalStatus="notMarried";
+
+console.log(typeof myName);
+console.log(typeof surname);
+console.log(typeof namegroup);
+console.log(typeof yearOfBirth);
+console.log(typeof surname);
+
+
+
+let x
+console.log(typeof x);
+let y
+console.log(typeof y);
